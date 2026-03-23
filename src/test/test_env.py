16acc30835cc9ec35c59c100d9203efa67f8b526
Trí Hoàn xyh <trihoan0908@gmail.com>
2026-03-24 00:23:19 +0700

    thay dổi và them data

--- src/test/test_env.py
+++
@@ -1,7 +0,0 @@
-try:
-    import sklearn
-    import pandas
-    import flask
-    print("✅ Chúc mừng! Môi trường học tập AI của bạn đã sẵn sàng.")
-except ImportError as e:
-    print(f"❌ Vẫn thiếu thư viện: {e}")
